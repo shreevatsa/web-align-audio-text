4707517165159c1e69bcc0a4d34ccd9ebb96ecb4
Shreevatsa R <shreevatsa.public@gmail.com>
2021-04-03 16:16:53 -0700

    Add the rest of PM, almost manually as there are only six of them

--- website/generate.sh
+++ website/generate.sh
@@ -1,4 +1,17 @@
 set -euo pipefail
+
+for pm in Pm01-10 Pm11-20 Pm21-30 Pm31-40 Pm41-50 Pm51-63; do
+    echo $pm
+    cat >content/meghaduta/$pm.md <<EOF
+---
+kind: "page"
+layout: "ramepage"
+alignmentjson: "meghaduta/word_alignment/$pm.json"
+alignmentaudio: "https://archive.org/download/meghadUta-mUlam-vedabhoomi.org/$pm.mp3"
+---
+EOF
+done
+
 for f in data/audio_alignment/ramayana/sentence_alignment/Kanda_*.json; do
     echo $f
     echo $f | python3 generate.py
